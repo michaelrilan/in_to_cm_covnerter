7ae7992faf6c2fdec3867b7e75b8cc4ad172bc83
michaelrilan <micorilan1999@gmail.com>
2023-01-31 01:48:49 +0800

    finished!111

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,19 +20,104 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.cm_txt = New System.Windows.Forms.TextBox()
+        Me.in_txt = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btn_convert = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'cm_txt
+        '
+        Me.cm_txt.Location = New System.Drawing.Point(66, 105)
+        Me.cm_txt.Name = "cm_txt"
+        Me.cm_txt.Size = New System.Drawing.Size(139, 20)
+        Me.cm_txt.TabIndex = 0
+        '
+        'in_txt
+        '
+        Me.in_txt.Location = New System.Drawing.Point(66, 153)
+        Me.in_txt.Name = "in_txt"
+        Me.in_txt.Size = New System.Drawing.Size(139, 20)
+        Me.in_txt.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(24, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(213, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Inch to Centimeter Converter"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(67, 89)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Enter Length(cm)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(67, 137)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Enter Length(in)"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(130, 15)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Lyonard Spencer Orap"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'btn_convert
+        '
+        Me.btn_convert.Location = New System.Drawing.Point(93, 195)
+        Me.btn_convert.Name = "btn_convert"
+        Me.btn_convert.Size = New System.Drawing.Size(75, 23)
+        Me.btn_convert.TabIndex = 6
+        Me.btn_convert.Text = "Convert"
+        Me.btn_convert.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(414, 384)
+        Me.ClientSize = New System.Drawing.Size(264, 259)
+        Me.Controls.Add(Me.btn_convert)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.in_txt)
+        Me.Controls.Add(Me.cm_txt)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Lyonard Spencer Orap"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents cm_txt As TextBox
+    Friend WithEvents in_txt As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btn_convert As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,28 @@
 ﻿Public Class Form1
 
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles cm_txt.TextChanged
+        in_txt.Text = 0
+
+    End Sub
+
+    Private Sub in_txt_TextChanged(sender As Object, e As EventArgs) Handles in_txt.TextChanged
+        cm_txt.Text = 0
+    End Sub
+
+    Private Sub btn_convert_Click(sender As Object, e As EventArgs) Handles btn_convert.Click
+        Dim cm_get As Double = cm_txt.Text
+
+        Dim in_get As Double = in_txt.Text
+        Dim formula_to_in As Double = cm_get * 0.393701
+        Dim formula_to_cm As Double = in_get * 2.54
+
+        If cm_get = 0 Then
+            MsgBox("Conversion to cm:" & formula_to_cm & "cm")
+        ElseIf in_get = 0 Then
+            MsgBox("Conversion to in:" & formula_to_in & "in")
+
+        End If
+
+
+    End Sub
 End Class
